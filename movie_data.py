c22eac938d6d2e7a2044574e1a149c95a3946e6e
grimreaperLinux <ani9431619703@gmail.com>
2022-03-30 16:04:20 +0530

    Adding the Data-Scraper

---
+++ movie_data.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.imdb.com/title/tt0120737/"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+print('Movie Name:',soup.h1.string)
+
+section = soup.find('section').find('section').contents[3]
+pagesection = section.find(class_ = "ipc-page-section").contents[2]
+a_tag = pagesection.contents[1].find("a", attrs={"aria-label":"View User Ratings"})
+ratings = a_tag.find("span").string
+print('Rating:', ratings)
+lowerhalf = soup.find('section').contents[1]
+castsection = lowerhalf.find("section", class_="title-cast")
+director = castsection.contents[2].contents[0].a.string
+writers = castsection.contents[2].contents[1].find_all("li")
+
+print('director:', director)
+for writer in writers:
+    print('Writers:', writer.a.string)
+#print(writers)
+
+stars = section.find_all("ul")[2].contents[2].div.find_all("li")
+
+for star in stars:
+    print('Cast:', star.a.string)
+
+genres = lowerhalf.find("section", attrs={"data-testid":"Storyline"}).find("li", attrs={"data-testid": "storyline-genres"}).find_all("li")
+
+for genre in genres:
+    print('Genres:',genre.a.string)
+
+ReleaseMonth = lowerhalf.find("section", attrs={"data-testid":"Details"}).li.li.a.string.split(" ", 1)[0]
+print('ReleaseMonth:', ReleaseMonth)
+languages = lowerhalf.find("section", attrs={"data-testid":"Details"}).find("li", attrs={"data-testid":"title-details-languages"}).find_all("li")
+moneymatters = budget = lowerhalf.find("section", attrs={"data-testid":"BoxOffice"})
+budget = moneymatters.li.li.span.string.split(" ", 1)[0]
+
+for language in languages:
+    print('Languages:', language.a.string)
+
+boxofficeUS = int(moneymatters.ul.contents[1].li.span.string[1:].replace(',',''))
+boxofficeWorld = int(moneymatters.ul.contents[3].li.span.string[1:].replace(',',''))
+print('BoxOffice:$',boxofficeUS + boxofficeWorld)
+
+print('Budget:',budget)
